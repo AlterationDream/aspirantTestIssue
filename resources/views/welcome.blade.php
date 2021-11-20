577d38de40dca35d62e25ba0a6b9dde02783870e
AlterationDream <mythical.form.of.life@gmail.com>
2021-11-20 19:26:07 +0800

    Добавил текущее время и дату на главную страницу.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,6 +45,19 @@
                     </svg>
                 </div>
 
+                <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
+
+                    <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
+                        Текущее время: <span id="current-time"></span>
+                    </div>
+                    <!--div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
+                        Текущее время: <span id="current-time"><?php echo Carbon\Carbon::now()->format('H:i:s');?></span>
+                    </div>
+                    <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
+                        Текущее время: <span id="current-time"><?php echo Carbon\Carbon::now()->format('H:i:s');?></span>
+                    </div-->
+                </div>
+
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
@@ -101,32 +114,29 @@
                     </div>
                 </div>
 
-                <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
-                    <div class="text-center text-sm text-gray-500 sm:text-left">
-                        <div class="flex items-center">
-                            <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="-mt-px w-5 h-5 text-gray-400">
-                                <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
-                            </svg>
 
-                            <a href="https://laravel.bigcartel.com" class="ml-1 underline">
-                                Shop
-                            </a>
-
-                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="ml-4 -mt-px w-5 h-5 text-gray-400">
-                                <path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
-                            </svg>
-
-                            <a href="https://github.com/sponsors/taylorotwell" class="ml-1 underline">
-                                Sponsor
-                            </a>
-                        </div>
-                    </div>
-
-                    <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
-                        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
-                    </div>
-                </div>
             </div>
         </div>
+        <script>
+            function showTime(){
+                let date = new Date();
+                let d = date.getDate();
+                let M = date.getMonth();
+                let y = date.getFullYear();
+                let h = date.getHours();
+                let m = date.getMinutes();
+                let s = date.getSeconds();
+                let monthA = 'января,февраля,марта,апреля,мая,июня,июля,августа,сентября,октября,ноября,декабря'.split(',');
+                h = (h < 10) ? "0" + h : h;
+                m = (m < 10) ? "0" + m : m;
+                s = (s < 10) ? "0" + s : s;
+
+                let time = h + ":" + m + ":" + s + " - " + d + " " + monthA[M] + " " + y;
+                document.getElementById("current-time").innerText = time;
+            }
+            showTime();
+            setInterval(showTime, 1000);
+
+        </script>
     </body>
 </html>
